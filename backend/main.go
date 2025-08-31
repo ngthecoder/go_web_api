94e19990e46c18f181f8be6a4fe1091f5a7db529
ngthecoder <ngoto0208@gmail.com>
2025-08-31 05:30:01 -0400

    findRecipesByIngredientsHandlerの実装

--- backend/main.go
+++ backend/main.go
@@ -609,7 +609,7 @@ func helloHandler(w http.ResponseWriter, r *http.Request) {
 func ingredientsHandler(w http.ResponseWriter, r *http.Request) {
 	rows, err := db.Query("SELECT * FROM ingredients")
 	if err != nil {
-		http.Error(w, "Database Error", http.StatusInternalServerError)
+		http.Error(w, "Database error", http.StatusInternalServerError)
 		return
 	}
 	defer rows.Close()
@@ -619,7 +619,7 @@ func ingredientsHandler(w http.ResponseWriter, r *http.Request) {
 		var ingredient Ingredient
 		err = rows.Scan(&ingredient.ID, &ingredient.Name, &ingredient.Category, &ingredient.Calories, &ingredient.Description)
 		if err != nil {
-			http.Error(w, "Data Scanning Error", http.StatusInternalServerError)
+			http.Error(w, "Data scanning error", http.StatusInternalServerError)
 		}
 		ingredients = append(ingredients, ingredient)
 	}
@@ -627,22 +627,22 @@ func ingredientsHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(map[string]interface{}{
 		"ingredients": ingredients,
-		"total":       len(ingredients),
 	})
 }
 
 func recipesHandler(w http.ResponseWriter, r *http.Request) {
 	rows, err := db.Query("SELECT * FROM recipes")
 	if err != nil {
-		http.Error(w, "Database Error", http.StatusInternalServerError)
+		http.Error(w, "Database error", http.StatusInternalServerError)
 	}
+	defer rows.Close()
 
 	var recipes []Recipe
 	for rows.Next() {
 		var recipe Recipe
 		err = rows.Scan(&recipe.ID, &recipe.Name, &recipe.Category, &recipe.PrepTimeMinutes, &recipe.CookTimeMinutes, &recipe.Servings, &recipe.Difficulty, &recipe.Instructions, &recipe.Description)
 		if err != nil {
-			http.Error(w, "Data Scanning Error", http.StatusInternalServerError)
+			http.Error(w, "Data scanning error", http.StatusInternalServerError)
 		}
 		recipes = append(recipes, recipe)
 	}
@@ -650,21 +650,17 @@ func recipesHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(map[string]interface{}{
 		"recipes": recipes,
-		"total":   len(recipes),
 	})
 }
 
 func findRecipesByIngredientsHandler(w http.ResponseWriter, r *http.Request) {
 	query := r.URL.Query()
 	ingredientsParams := query.Get("ingredients")
-	fmt.Println(ingredientsParams)
 	matchType := query.Get("match_type")
-	fmt.Println(matchType)
 	limitParams := query.Get("limit")
-	fmt.Println(limitParams)
 
 	if ingredientsParams == "" {
-		http.Error(w, "Missing Required Parameters: ingredients", http.StatusBadRequest)
+		http.Error(w, "Missing required parameters: ingredients", http.StatusBadRequest)
 		return
 	}
 
@@ -694,11 +690,97 @@ func findRecipesByIngredientsHandler(w http.ResponseWriter, r *http.Request) {
 	}
 
 	// SQL Query here
+	sqlQuery := ""
+	args := []interface{}{}
+
+	placeholders := make([]string, 0, len(ingredientIDs))
+	for _, ingredientID := range ingredientIDs {
+		placeholders = append(placeholders, "?")
+		args = append(args, ingredientID)
+	}
+	args = append(args, limit)
+
+	if matchType == "partial" {
+		sqlQuery = fmt.Sprintf(
+			`
+			SELECT r.id, r.name, r.category, r.prep_time_minutes, r.cook_time_minutes, r.servings, r.difficulty, r.instructions, r.description,
+			COUNT(ri.ingredient_id) as match_ingredients_count,
+			(SELECT COUNT(*) FROM recipe_ingredients WHERE recipe_id = r.id) as total_ingredients_count
+			FROM recipes r
+			JOIN recipe_ingredients ri on r.id = ri.recipe_id
+			WHERE ri.ingredient_id in (%s)
+			GROUP BY r.id, r.name, r.category, r.prep_time_minutes, r.cook_time_minutes, r.servings, r.difficulty, r.instructions, r.description
+			ORDER BY match_ingredients_count DESC, total_ingredients_count ASC
+			LIMIT ?
+		`, strings.Join(placeholders, ","))
+	}
+
+	if matchType == "exact" {
+		sqlQuery = fmt.Sprintf(
+			`
+			SELECT r.id, r.name, r.category, r.prep_time_minutes, r.cook_time_minutes, r.servings, r.difficulty, r.instructions, r.description,
+			COUNT(ri.ingredient_id) as match_ingredients_count,
+			(SELECT COUNT(*) FROM recipe_ingredients WHERE recipe_id = r.id) as total_ingredients_count
+			FROM recipes r
+			JOIN recipe_ingredients ri on r.id = ri.recipe_id
+			WHERE ri.ingredient_id in (%s)
+			GROUP BY r.id, r.name, r.category, r.prep_time_minutes, r.cook_time_minutes, r.servings, r.difficulty, r.instructions, r.description
+			HAVING COUNT(ri.ingredient_id) = (SELECT COUNT(*) FROM recipe_ingredients WHERE recipe_id = r.id)
+			ORDER BY match_ingredients_count DESC, total_ingredients_count ASC
+			LIMIT ?
+		`, strings.Join(placeholders, ","))
+	}
+
+	rows, err := db.Query(sqlQuery, args...)
+	if err != nil {
+		log.Printf("SQL Error: %v", err)
+		http.Error(w, "Database error", http.StatusInternalServerError)
+		return
+	}
+	defer rows.Close()
+
+	type MatchedRecipe struct {
+		ID                      int     `json:"id"`
+		Name                    string  `json:"name"`
+		Category                string  `json:"category"`
+		PrepTimeMinutes         int     `json:"prep_time_minutes"`
+		CookTimeMinutes         int     `json:"cook_time_minutes"`
+		Servings                int     `json:"servings"`
+		Difficulty              string  `json:"difficulty"`
+		Instructions            string  `json:"instructions"`
+		Description             string  `json:"description"`
+		MatchedIngredientsCount int     `json:"matched_ingredients_count"`
+		TotalIngredientsCount   int     `json:"total_ingredients_count"`
+		MatchScore              float32 `json:"match_score"`
+	}
+
+	matchedRecipes := []MatchedRecipe{}
+	for rows.Next() {
+		var matchedRecipe MatchedRecipe
+		err = rows.Scan(
+			&matchedRecipe.ID,
+			&matchedRecipe.Name,
+			&matchedRecipe.Category,
+			&matchedRecipe.PrepTimeMinutes,
+			&matchedRecipe.CookTimeMinutes,
+			&matchedRecipe.Servings,
+			&matchedRecipe.Difficulty,
+			&matchedRecipe.Instructions,
+			&matchedRecipe.Description,
+			&matchedRecipe.MatchedIngredientsCount,
+			&matchedRecipe.TotalIngredientsCount,
+		)
+		matchedRecipe.MatchScore = float32(matchedRecipe.MatchedIngredientsCount) / float32(matchedRecipe.TotalIngredientsCount)
+		if err != nil {
+			http.Error(w, "Database scanning error", http.StatusInternalServerError)
+			return
+		}
+		matchedRecipes = append(matchedRecipes, matchedRecipe)
+	}
 
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(map[string]interface{}{
-		"message":              fmt.Sprintf("Looking for recipes with ingredients: %v, match_type: %s, limit: %d", ingredientIDs, matchType, limit),
-		"ingredients_received": ingredientIDs,
+		"matched_recipes": matchedRecipes,
 	})
 }
 

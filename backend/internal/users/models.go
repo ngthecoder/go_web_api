2e17ccabccfec4afeec0869aee2640cb610a0b10
ngthecoder <ngoto0208@gmail.com>
2025-09-28 19:35:02 -0400

    defined user structs

--- backend/internal/users/models.go
+++ backend/internal/users/models.go
@@ -1,1 +1,19 @@
 package users
+
+import "time"
+
+type UserProfile struct {
+	ID        string    `json:"id"`
+	Username  string    `json:"username"`
+	Email     string    `json:"email"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+}
+
+type LikedRecipeRequest struct {
+	RecipeID int `json:"recipe_id"`
+}
+
+type LikedRecipeResponse struct {
+	RecipeIDs []int `json:"recipe_ids"`
+}
